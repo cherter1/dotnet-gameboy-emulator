d066e3d7c66f9077820b75d8040eccc7313aeddf
Matthew Anderson <matthewa054@gmail.com>
2026-07-01 11:50:07 -0400

    start format5 tests

--- tests/GbaEmulator.Core.Tests/Cpu/ThumbMode/Format4AluOps.cs
+++ tests/GbaEmulator.Core.Tests/Cpu/ThumbMode/Format4AluOps.cs
@@ -6,25 +6,6 @@ namespace GbaEmulator.Core.Tests.Cpu.ThumbMode;
 
 public sealed class Format4AluOps
 {
-    /*
-     temp instruction comment
-      2000000:       4008            ands    r0, r1
-      2000002:       4048            eors    r0, r1
-      2000004:       4088            lsls    r0, r1
-      2000006:       40c8            lsrs    r0, r1
-      2000008:       4108            asrs    r0, r1
-      200000a:       4148            adcs    r0, r1
-      200000c:       4188            sbcs    r0, r1
-      200000e:       41c8            rors    r0, r1
-      2000010:       4208            tst     r0, r1
-      2000012:       4248            negs    r0, r1
-      2000014:       4288            cmp     r0, r1
-      2000016:       42c8            cmn     r0, r1
-      2000018:       4308            orrs    r0, r1
-      200001a:       4348            muls    r0, r1
-      200001c:       4388            bics    r0, r1
-      200001e:       43c8            mvns    r0, r1 
-     */
     [Fact]
     public void AND_OperandsDontShareAnyHighBits_ZeroSetCVUnchanged()
     {
